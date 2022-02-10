2e4e4bac37ae22ff4f6c818fbb3a3cc17dd3873b
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2022-02-10 18:21:28 +0000

    Config Updates

--- src/main/java/net/zestyblaze/capybara/config/CapybaraModConfig.java
+++ src/main/java/net/zestyblaze/capybara/config/CapybaraModConfig.java
@@ -3,13 +3,19 @@ package net.zestyblaze.capybara.config;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import net.zestyblaze.capybara.Capybara;
 
 @Config(name = Capybara.MODID)
 public class CapybaraModConfig implements ConfigData {
+
+    @ConfigEntry.Gui.RequiresRestart
     public boolean debugMode = false;
+
     public int spawnWeight = 3;
+
     public int minGroupSize = 1;
+
     public int maxGroupSize = 4;
 
     public static CapybaraModConfig get() {
