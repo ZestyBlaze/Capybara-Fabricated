6a4734aaa715551f9f771e1d33716b6e312a6b5b
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2022-06-25 20:35:15 +0100

    1.19 update

--- src/main/java/net/zestyblaze/capybara/registry/CapybaraWorldInit.java
+++ src/main/java/net/zestyblaze/capybara/registry/CapybaraWorldInit.java
@@ -3,13 +3,13 @@ package net.zestyblaze.capybara.registry;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.world.biome.Biome;
+import net.minecraft.tag.BiomeTags;
 import net.zestyblaze.capybara.Capybara;
 import net.zestyblaze.capybara.config.CapybaraModConfig;
 
 public class CapybaraWorldInit {
     public static void registerSpawns() {
-        BiomeModifications.addSpawn(BiomeSelectors.categories(Biome.Category.JUNGLE), SpawnGroup.CREATURE, CapybaraEntityInit.CAPYBARA, CapybaraModConfig.get().spawnWeight, CapybaraModConfig.get().minGroupSize, CapybaraModConfig.get().maxGroupSize);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_JUNGLE), SpawnGroup.CREATURE, CapybaraEntityInit.CAPYBARA, CapybaraModConfig.get().spawnWeight, CapybaraModConfig.get().minGroupSize, CapybaraModConfig.get().maxGroupSize);
 
         if(CapybaraModConfig.get().debugMode) {
             Capybara.LOGGER.info("Capybara: Registry - Entity Spawns Registered");
