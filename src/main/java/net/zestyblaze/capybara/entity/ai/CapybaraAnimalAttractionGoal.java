ed9ac633e9c664629c74be5bad84dc95a1b3a603
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2022-09-15 12:07:05 +0100

    Fix #10

--- src/main/java/net/zestyblaze/capybara/entity/ai/CapybaraAnimalAttractionGoal.java
+++ src/main/java/net/zestyblaze/capybara/entity/ai/CapybaraAnimalAttractionGoal.java
@@ -2,6 +2,7 @@ package net.zestyblaze.capybara.entity.ai;
 
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.TameableEntity;
 
 public class CapybaraAnimalAttractionGoal extends Goal {
     private final MobEntity entity;
@@ -24,7 +25,7 @@ public class CapybaraAnimalAttractionGoal extends Goal {
     public void start() {
         super.start();
         for(MobEntity mobEntity : entity.world.getEntitiesByClass(MobEntity.class, entity.getBoundingBox().expand(5), e -> e != entity && e.getVehicle() == null)) {
-            if(mobEntity.getWidth() <= 0.75f && mobEntity.getHeight() <= 0.75f) {
+            if(mobEntity.getWidth() <= 0.75f && mobEntity.getHeight() <= 0.75f && !((TameableEntity)mobEntity).isSitting()) {
                 mobEntity.getNavigation().startMovingTo(entity, mobEntity.getMovementSpeed() + 0.4);
             }
         }
